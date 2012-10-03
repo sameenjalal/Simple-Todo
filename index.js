5c258472491c940c50f21b8c472bdcd59ce34e0f
Sameen Jalal <sameenjalal@gmail.com>
2012-10-03 20:11:19 +0000

    asdf

--- index.js
+++
@@ -1,9 +0,0 @@
-var server = require("./server")
-  ,router = require("./router")
-  ,requestHandler = require("./requestHandler");
-
-var handle = {}
-handle["/"] = requestHandler.todo;
-handle["/todo"] = requestHandler.todo;
-
-server.start(router.route, handle);
--- requestHandler.js
+++
@@ -1,7 +0,0 @@
-function todo(res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.write("Hello World");
-  res.end();
-}
-
-exports.todo = todo;
